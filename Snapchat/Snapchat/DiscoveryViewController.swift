9aaf35947946173005b2e56c5862ad9b7dc03ba8
Can <504461918@qq.com>
2016-10-04 11:48:07 +1100

    new commit

--- Snapchat/Snapchat/DiscoveryViewController.swift
+++ Snapchat/Snapchat/DiscoveryViewController.swift
@@ -15,22 +15,31 @@ class DiscoveryViewController: UITableViewController, UIGestureRecognizerDelegat
         self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         
         let target = self.navigationController?.interactivePopGestureRecognizer!.delegate
-        
-
-        
         let pan = UIPanGestureRecognizer(target:target,
                                          action: Selector("handleNavigationTransition:"))
         pan.delegate = self
-        self.view.addGestureRecognizer(pan)
+        self.tableView.addGestureRecognizer(pan)
         self.navigationController?.interactivePopGestureRecognizer!.enabled = false
     }
     
-//    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-//        if self.childViewControllers.count == 1 {
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if self.childViewControllers.count == 1 {
+            return false
+        }
+        return true
+        
+    }
+    
+//    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+//        return false
+//    }
+ 
+//    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOfGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//        if let pan = gestureRecognizer as? UIPanGestureRecognizer {
+//            if let scroll = gestureRecognizer as? uis
 //            return false
 //        }
-//        return true
-//        
+//        return false
 //    }
     
     
